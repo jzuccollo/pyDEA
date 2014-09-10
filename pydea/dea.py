97618f6eead5b940cbfea988399b94595b27c667
James Zuccollo <james.zuccollo@reform.co.uk>
2014-09-10 18:24:42 +0100

    PEP8 fixes

--- pydea/dea.py
+++ pydea/dea.py
@@ -1,4 +1,5 @@
-# The core DEA class, setting up and solving the linear programming problems using PuLP.
+# The core DEA class, setting up and solving the linear programming
+# problems using PuLP.
 
 
 import numpy as np
@@ -7,83 +8,82 @@ import pulp
 
 
 class DEA:
+
     """
     A container for the elements of a data envelopment analysis problem. Sets up the linear programmes and solves them with pulp.
-    
+
     Requires:
-    
+
         inputs: a pandas dataframe of the inputs to the DMUs
         outputs: a pandas dataframe of the outputs from the DMUs
         kind: 'VRS' or 'CRS'
         in_weights: the weight restriction to apply to all inputs to all DMUs (default is [0, inf])
         out_weights: the weight restriction to apply to all outputs to all DMUs (default is [0, inf)
-        
+
     Weight restrictions must be specified as a list. To specify only one bound leave the other as None, eg. in_weights=[1, None].
-        
+
     """
 
-    
     def __init__(self, inputs, outputs, returns='CRS', in_weights=[0, None], out_weights=[0, None]):
         """
         Set up the DMUs' problems, ready to solve.
-        
+
         """
         self.inputs = self._to_dataframe(inputs)
         self.outputs = self._to_dataframe(outputs)
         self.returns = returns
-               
+
         self.J, self.I = self.inputs.shape  # no of firms, inputs
         _, self.R = self.outputs.shape  # no of outputs
         self._i = np.arange(self.I)  # inputs
         self._r = np.arange(self.R)  # outputs
         self._j = np.arange(self.J)  # DMUs
-        
+
         self._in_weights = in_weights
         self._out_weights = out_weights
 
-        self.dmus = self._create_problems()  # creates dictionary of pulp.LpProblem objects for the DMUs
+        # creates dictionary of pulp.LpProblem objects for the DMUs
+        self.dmus = self._create_problems()
 
-    
     def _to_dataframe(self, indata):
         """
         Indexers require input to be a dataframe but the user may pass a series. Check and cast series to dataframes.
-        
+
         """
-        
+
         if type(indata) == pd.core.frame.DataFrame:
             return indata
         elif type(indata) == pd.core.series.Series:
             return pd.DataFrame(indata)
         else:
-            raise TypeError("Input data is not a valid pandas DataFrame or Series.")
-        
-    
+            raise TypeError(
+                "Input data is not a valid pandas DataFrame or Series.")
+
     def _create_problems(self):
         """
         Iterate over the inputs and create a dictionary of LP problems, one for each DMU.
-        
+
         """
-        
+
         dmu_dict = {}
         for j0 in self._j:
             dmu_dict[j0] = self._make_problem(j0)
         return dmu_dict
- 
-    
+
     def _make_problem(self, j0):
         """
         Create a pulp.LpProblem for a DMU.
-        
+
         """
-        
-        ##Set up pulp
+
+        # Set up pulp
         prob = pulp.LpProblem("".join(["DMU_", str(j0)]), pulp.LpMaximize)
-        self.inputWeights = pulp.LpVariable.dicts("inputWeight", (self._j, self._i), 
+        self.inputWeights = pulp.LpVariable.dicts("inputWeight", (self._j, self._i),
                                                   lowBound=self._in_weights[0], upBound=self._in_weights[1])
-        self.outputWeights = pulp.LpVariable.dicts("outputWeight", (self._j, self._r), 
+        self.outputWeights = pulp.LpVariable.dicts("outputWeight", (self._j, self._r),
                                                    lowBound=self._out_weights[0], upBound=self._out_weights[1])
-                
-        ##Set returns to scale
+
+        # Set returns to scale
         if self.returns == "CRS":
             w = 0
         elif self.returns == "VRS":
@@ -91,35 +91,37 @@ class DEA:
         else:
             raise Exception(ValueError)
 
-        ##Set up objective function
-        prob += pulp.LpAffineExpression([(self.outputWeights[j0][r1], self.outputs.values[j0][r1]) for r1 in self._r]) - w
+        # Set up objective function
+        prob += pulp.LpAffineExpression(
+            [(self.outputWeights[j0][r1], self.outputs.values[j0][r1]) for r1 in self._r]) - w
 
-        ##Set up constraints
-        prob += pulp.LpAffineExpression([(self.inputWeights[j0][i1], self.inputs.values[j0][i1]) for i1 in self._i]) == 1, "Norm_constraint"
+        # Set up constraints
+        prob += pulp.LpAffineExpression([(self.inputWeights[j0][i1], self.inputs.values[
+                                        j0][i1]) for i1 in self._i]) == 1, "Norm_constraint"
         for j1 in self._j:
-            prob += self._dmu_constraint(j0, j1)  - w <= 0, "".join(["DMU_constraint_", str(j1)])
+            prob += self._dmu_constraint(j0, j1) - \
+                w <= 0, "".join(["DMU_constraint_", str(j1)])
         return prob
-    
-    
+
     def _dmu_constraint(self, j0, j1):
         """
         Calculate and return the DMU constraint for a single DMU's LP problem.
-        
-        """
-        
-        eOut = pulp.LpAffineExpression([(self.outputWeights[j0][r1], self.outputs.values[j1][r1]) for r1 in self._r])
-        eIn = pulp.LpAffineExpression([(self.inputWeights[j0][i1], self.inputs.values[j1][i1]) for i1 in self._i])
-        return eOut - eIn     
 
-    
+        """
+
+        eOut = pulp.LpAffineExpression(
+            [(self.outputWeights[j0][r1], self.outputs.values[j1][r1]) for r1 in self._r])
+        eIn = pulp.LpAffineExpression(
+            [(self.inputWeights[j0][i1], self.inputs.values[j1][i1]) for i1 in self._i])
+        return eOut - eIn
+
     def _solver(self):
         """
         Iterate over the dictionary of DMUs' problems, solve them, and collate the results into a pandas dataframe.
-        
+
         """
-        
+
         sol_status = {}
-        sol_results = {}
         sol_weights = {}
         sol_efficiency = {}
         for ind, problem in self.dmus.iteritems():
@@ -130,8 +132,12 @@ class DEA:
                 sol_weights[ind][v.name] = v.varValue
             sol_efficiency[ind] = pulp.value(problem.objective)
         return pd.DataFrame.from_dict({'Status': sol_status, 'Efficiency': sol_efficiency, 'Weights': sol_weights})
-        
-    
+
     def solve(self):
+        """"
+        Solve the problem and create attributes to hold the solutions.
+
+        """
+
         self.results = self._solver()
         self.results.index = self.inputs.index
--- pydea/tools.py
+++ pydea/tools.py
@@ -4,35 +4,41 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def env_corr(dea_obj, env_vars):
     """
     Determine correlations with environmental/non-discretionary variables using a logit regression. Tobit will be implemented when available upstream in statsmodels.
-    
+
     Takes:
         dea_obj: The solved DEA instance.
-        env_vars: A pandas dataframe of 
-        
+        env_vars: A pandas dataframe of
+
     Returns:
         corr_mod: the statsmodels' model instance containing the inputs and results from the logit model.
-    
+
     Note that there can be no spaces in the variables' names.
     """
-    
+
     print "A logit regression will be used. A censored regression with a Tobit model would be more correct but it is not yet provided by statsmodels.\n"
-    
-    import statsmodels.api as sm
+
     from statsmodels.formula.api import logit
-    
+
     corr_data = env_vars.join(dea_obj.results['Efficiency'])
-    corr_mod = logit("Efficiency ~ " + " + ".join(env_vars.columns), corr_data).fit()
-    
+    corr_mod = logit(
+        "Efficiency ~ " + " + ".join(env_vars.columns), corr_data).fit()
+
     mfx = corr_mod.get_margeff()
     print mfx.summary()
-    
+
     return corr_mod
 
 
 def normalise_df(df):
+    """
+    Normalise the series in a dataframe to z-scores,
+
+    """
+
     dfnorm = pd.DataFrame(index=df.index)
     for ind, ser in df.iteritems():
         dfnorm[ind] = (ser - ser.mean()) / ser.std()
@@ -42,30 +48,32 @@ def normalise_df(df):
 def deaPCA(df, allres=False, normalise=False, plot=True):
     """
     Extract principal components from pandas dataframe and shift distribution so that all values are strictly positive, as required for DEA.
-    
+
     Takes:
         df: A dataframe of series to run the PCA on.
         allres: Boolean. Set True if you would like to get the PCA object returned instead of the transformed data. This can be useful if you wish to use the entire results of the PCA.
         normalise: Boolean. Set True to normalise the series to a z-score before transforming.
         plot: Should the function display a plot of the variance explained?
     """
-    
+
     from sklearn.decomposition import PCA as sklearnPCA
 
     if normalise:
         df = normalise_df(df)
-    
+
     indat_pca = sklearnPCA()
-    indat_transf = pd.DataFrame(indat_pca.fit_transform(df.values), index=df.index)
+    indat_transf = pd.DataFrame(
+        indat_pca.fit_transform(df.values), index=df.index)
 
     for ser, vals in indat_transf.iteritems():
-        if vals.min() <=0:
+        if vals.min() <= 0:
             indat_transf[ser] = vals + np.abs(vals.min()) + 1
-   
+
     if plot:
-        fig1, ax1 = plt.subplots()
+        _, ax1 = plt.subplots()
         ax1.plot(np.array(indat_pca.explained_variance_ratio_).cumsum())
-        ax1.bar(np.arange(0.1, len(indat_pca.explained_variance_ratio_), 1), np.array(indat_pca.explained_variance_ratio_))
+        ax1.bar(np.arange(0.1, len(indat_pca.explained_variance_ratio_), 1), np.array(
+            indat_pca.explained_variance_ratio_))
         ax1.set_title('Variance explained by each principal component')
         ax1.set_xlim(right=len(indat_pca.explained_variance_ratio_))
         ax1.set_ylim(top=1)
