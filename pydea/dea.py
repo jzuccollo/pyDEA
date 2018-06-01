ca18adafbe16e2cafa07de97dc0fd687c9dac74c
pomcho555 <wundtjobber18@yahoo.co.jp>
2018-06-02 02:15:11 +0900

    Fix _build_weight_results_dict method (#10)
    * Fix _build_weight_results_dict method

--- pydea/dea.py
+++ pydea/dea.py
@@ -131,15 +131,16 @@ class DEAProblem:
         pandas dataframe of all weights.
 
         """
+        import re
         tmp_dict = {}
         for dmu, d in list(sol_weights.items()):
             tmp_dict[dmu] = {}
             for key, _ in list(d.items()):
+                m = re.search(r'[0-9]+$',key)
+                i = int(m.group(0))
                 if key.startswith("input"):
-                    i = int(key[-1])
                     tmp_dict[dmu]["in_" + str(self.inputs.columns[i])] = d[key]
                 if key.startswith("output"):
-                    i = int(key[-1])
                     tmp_dict[dmu][
                         "out_" + str(self.outputs.columns[i])] = d[key]
         weight_results = pd.DataFrame.from_dict(tmp_dict).T
